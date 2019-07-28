c819b885a16b5ff5b176a0da8e136ca393b8687c
ganesh <dganeshdilli@gmail.com>
2019-07-29 00:23:47 +0530

    added crud to script

--- myscript.js
+++ myscript.js
@@ -1,18 +1,72 @@
 $(document).ready(function(){
-    // var listOfStudents = [];
-    console.log("**",listOfStudents)
+    var listOfStudents = [];
+    var edit = false;
+    var editIndex;
+    // console.log("**",listOfStudents)
     $('#add').on('click',function(event){
         event.preventDefault();
-       
         let studentName = {}
-        studentName['name']=$('#inputName').val();
-        studentName['age']=$('#inputAge').val();
-        studentName['standard']=$('#inputStandard').val();
-        console.log("studentName",studentName)
-
-        $('tbody').append("<tr><th>"+studentName.name+"</th><th>"+studentName.age+"</th><th>"+studentName.standard+"</th></tr>")
-        // listOfStudents.push(studentName);
-        $('form :input').val('');
-        console.log("Listofstudents",listOfStudents)
+            studentName['name']=$('#inputName').val();
+            studentName['age']=$('#inputAge').val();
+            studentName['standard']=$('#inputStandard').val();
+        // check edit or add
+        if(!edit){
+            // enters this block when u click submit
+            listOfStudents.push(studentName);
+            $('form :input').val('');
+            listAllStudents(listOfStudents)
+        }
+        else{
+            // enters this block when u click update
+            listOfStudents.splice(editIndex,1,studentName)
+            $('form :input').val('');
+            $('#add').html('SUBMIT');
+            edit = false;
+            listAllStudents(listOfStudents)
+        }
+        
+        
+        // console.log("Listofstudents",listOfStudents)
     })
+
+    // edit student
+    $("tbody").on('click','#edit',function(event){
+    console.log(this,$(this).data("index"));
+    let index = $(this).data("index");
+    populateStudentData(listOfStudents[index])
+    edit = true;
+    editIndex = index;
+    })
+
+    // delete student
+    $("tbody").on('click','#delete',function(event){
+        console.log(this,$(this).data("index"));
+        let index = $(this).data("index");
+        listOfStudents.splice(index,1);
+        listAllStudents(listOfStudents)
+        })
 })
+
+
+// function that takes list of students as a array and appends the students data to DOM
+function listAllStudents(studentList){
+    console.log(studentList)
+    $("tbody").empty();
+    studentList.forEach((student,i)=>{
+        console.log(i,student);
+        $('tbody').append("<tr><th>"+student.name+"</th><th>"+
+        student.age+"</th><th>"+
+        student.standard+"</th>"+
+        "<th> <button class='btn btn-primary' id='delete' data-index="+i+">delete</button></th>"+
+        "<th> <button class='btn btn-primary' id='edit' data-index="+i+">edit</button></th></tr>")
+    })
+}
+
+// function that takes individual student data and populates to form field
+function populateStudentData(student){
+    console.log(student)
+    $('#inputName').val(student.name)
+    $('#inputAge').val(student.age);
+    $('#inputStandard').val(student.standard);
+    $('#add').html('UPDATE') 
+}
